0046d6f28d48e6487657d21d5ee1290bfcd1f5fa
Zeph180 <zephrichards1@gmail.com>
2023-06-18 18:17:46 +0300

    Add tools svgs

---
+++ src/assets/tools/JiraIcon.js
@@ -0,0 +1,35 @@
+import * as React from "react"
+const JiraIcon
+ = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={60}
+    height={60}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3}
+      d="M6.875 28.715a10.92 10.92 0 0 0 10.92 10.92h2.57v2.57a10.92 10.92 0 0 0 10.92 10.92v-24.41H6.875Z"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3}
+      d="M17.795 17.795a10.92 10.92 0 0 0 10.92 10.92h2.57v2.57a10.92 10.92 0 0 0 10.92 10.92v-24.41h-24.41Z"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3}
+      d="M28.715 6.875a10.92 10.92 0 0 0 10.92 10.92h2.57v2.57a10.92 10.92 0 0 0 10.92 10.92V6.875h-24.41Z"
+    />
+  </svg>
+)
+export default JiraIcon
+
---
+++ src/assets/tools/index.js
@@ -0,0 +1,23 @@
+import CssIcon from "./CssIcon";
+import HtmlIcon from "./HtmlIcon";
+import JsIcon from "./JsIcon";
+import ReactIcon from "./ReactIcon";
+import JiraIcon from "./JiraIcon";
+import FigmaIcon from "./FigmaIcon";
+import GraphIcon from "./GraphIcon";
+import NodeIcon from "./NodeIcon";
+import MongoIcon from "./MongoIcon";
+import VscodeIcon from "./VscodeIcon";
+
+export {
+  CssIcon,
+  HtmlIcon,
+  JsIcon,
+  ReactIcon,
+  JiraIcon,
+  FigmaIcon,
+  GraphIcon,
+  NodeIcon,
+  MongoIcon,
+  VscodeIcon,
+}
